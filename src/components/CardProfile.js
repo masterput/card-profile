53c54c63418ac08d5aa362910a2be108b0fa6796
masput <prajaputra98@gmail.com>
2022-11-25 16:34:28 +0700

    create CardProfile

---
+++ src/components/CardProfile.js
@@ -0,0 +1,15 @@
+const CardProfile = () => {
+    return (
+        <div>
+            <div>
+                <img />
+                <div>Front End Developer</div>
+                <div>Master Putra Praja</div>
+                <div>Have idea to create some beautiful web</div>
+                <button>Contact Me</button>
+            </div>
+        </div>
+    )
+}
+
+export default CardProfile
